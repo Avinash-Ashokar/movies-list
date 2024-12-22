8b3d1778e9ed0c5854039fe5a7a7fcef39d64978
Avinash-Ashokar <mail_avinashashokar@pm.me>
2024-12-21 19:32:11 -0600

    Implemented base input field component

---
+++ src/components/input-field/index.tsx
@@ -0,0 +1,22 @@
+import { InputFieldProps } from "@/types";
+
+export default function InputField({
+  placeholderText,
+  inputData,
+  setInputData,
+}: InputFieldProps) {
+  const handleInputChange = (value: string) => {
+    setInputData(value);
+  };
+
+  return (
+    <div>
+      <input
+        placeholder={placeholderText}
+        value={inputData}
+        onChange={(e) => handleInputChange(e.target.value)}
+        className="bg-[#224957] min-w-full sm:w-[300px] h-[45px] rounded-[10px] px-4 focus:outline-none text-sm text-[#FFFFFF] font-normal leading-6"
+      />
+    </div>
+  );
+}
