f69ad41d1974e53ca15e3c7749a0c76c0ebc664f
Avinash-Ashokar <mail_avinashashokar@pm.me>
2024-12-21 18:32:09 -0600

    Configured tailwindcss for the given color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        primary: '#2BD17E',
+        error: '#EB5757',
+        background: '#093545',
+        input: '#224957',
+        card: '#092C39',
+      }
     },
   },
   plugins: [],
